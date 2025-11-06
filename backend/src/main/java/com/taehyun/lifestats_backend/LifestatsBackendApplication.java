b9d7d43d7ac48a98afc24b07cdbefb52105bfe9f
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-11-06 14:51:13 +0900

    Init: backend setup

---
+++ backend/src/main/java/com/taehyun/lifestats_backend/LifestatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taehyun.lifestats_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestatsBackendApplication.class, args);
+	}
+
+}
