c76ec8fdaae5f4922a86c4e0986d41a724bf84cc
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-11-17 11:40:40 +0900

    Feat: 프로필 정보 입력 페이지 구조 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
-import Dashboard from '@/components/pages/dashboard'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "@/components/pages/dashboard";
+import ProfileForm from "@/components/pages/profileForm";
+import "./App.css";
 
 function App() {
-
   return (
-    <Dashboard />
-  )
+    <Router>
+      <Routes>
+        <Route path="/profileForm" element={<ProfileForm />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/pages/profileForm.tsx
@@ -0,0 +1,7 @@
+import { ProfileDialog } from "../profile-dialog";
+
+export default function ProfileForm() {
+    return (
+        <ProfileDialog />
+    );
+}
