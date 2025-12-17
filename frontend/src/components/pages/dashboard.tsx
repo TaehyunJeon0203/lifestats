4dae33267e2e050fab04504528674881628ab8bd
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-12-18 04:00:49 +0900

    Delete: 사이드바 비활성화

--- frontend/src/components/pages/dashboard.tsx
+++ frontend/src/components/pages/dashboard.tsx
@@ -148,10 +148,8 @@ export default function Dashboard() {
 
   return (
     <SidebarProvider>
-      <AppSidebar profile={profile} stats={stats} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
           <Separator
             orientation="vertical"
             className="mr-2 data-[orientation=vertical]:h-4"
